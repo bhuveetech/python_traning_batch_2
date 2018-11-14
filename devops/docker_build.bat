8f52cff819c2980b85664748a930d85c6b1eebaf
Naresh <naresh.e0519@gmail.com>
2018-11-14 22:02:58 +0530

    Updated devops files

--- devops/docker_build.bat
+++ devops/docker_build.bat
@@ -1,4 +1,4 @@
-docker build -t sample_webapp .
-docker image ls
+docker build -t sample_webapp . &
+docker image ls &
 docker run --name web_application -p 5000:8080 -d  sample_webapp
 
--- devops/unit_tests.bat
+++ devops/unit_tests.bat
@@ -1,3 +1,3 @@
-C:\Users\Naresh\PycharmProjects\python_traning_batch_2\venv\Scripts\activate
-cd source\TestApplication\unittests
+C:\Users\Naresh\PycharmProjects\python_traning_batch_2\venv\Scripts\activate &
+cd source\TestApplication\unittests &
 pytest -v app_tests.py
